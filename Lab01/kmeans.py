b3a7b696aca569682203b3a0469418dfc313eb40
PeilinZhong <31450172+PeilinzhongZ@users.noreply.github.com>
2019-03-19 09:57:43 -0700

    update

--- Lab01/kmeans.py
+++ Lab01/kmeans.py
@@ -1,7 +1,6 @@
 from cluster import cluster
 from copy import deepcopy
 import numpy as np
-from sklearn.datasets.samples_generator import make_blobs 
 
 class KMeansCluster(cluster):
 
@@ -16,7 +15,7 @@ class KMeansCluster(cluster):
         std = np.std(X)
         centroids = std * np.random.randn(self.k, c) + mean
         centroids_pre = np.zeros(centroids.shape)
-        clusters = np.zeros(n)
+        clusters = np.zeros(n, dtype=int)
         iter_Num = 0
         while iter_Num < self.max_iterations:
             for i in range(n):
@@ -28,15 +27,10 @@ class KMeansCluster(cluster):
                         clusters[i] = t
             centroids_pre = deepcopy(centroids)
             for k in range(self.k):
-                centroids[k] = np.mean(X[clusters == k], axis=0)
+                temp = X[clusters == k]
+                if len(temp) != 0:
+                    centroids[k] = np.mean(temp, axis=0)                
             iter_Num += 1
             if np.linalg.norm(centroids - centroids_pre) == 0:
                 break
         return clusters, centroids
-        
-X, cluster_assignments = make_blobs(n_samples=200, centers=4, cluster_std=0.60, random_state=0)
-test = KMeansCluster(4)
-clusters, centroids = test.fit(X)
-print(clusters)
-print(centroids)
-print(cluster_assignments)
