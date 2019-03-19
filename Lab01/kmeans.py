fccf1217f66654b42b110b450bc367a255454116
PeilinZhong <31450172+PeilinzhongZ@users.noreply.github.com>
2019-03-19 13:06:40 -0700

    comment

--- Lab01/kmeans.py
+++ Lab01/kmeans.py
@@ -9,15 +9,18 @@ class KMeansCluster(cluster):
         self.max_iterations = max_iterations
 
     def fit(self, X):
+        # random k centroids
         n = X.shape[0]
         c = X.shape[1]
         mean = np.mean(X, axis=0)
         std = np.std(X, axis=0)
         centroids = std * np.random.randn(self.k, c) + mean
+        # initial clusters and previous centroids
         centroids_pre = np.zeros(centroids.shape)
         clusters = np.zeros(n, dtype=int)
         iter_Num = 0
         while iter_Num < self.max_iterations:
+            # calculate and store the index of closest centroid for each data point
             for i in range(n):
                 dist_min = np.linalg.norm(X[i] - centroids[0])
                 for t in range(self.k):
@@ -25,12 +28,15 @@ class KMeansCluster(cluster):
                     if dist <= dist_min:
                         dist_min = dist
                         clusters[i] = t
+            # update the old centroids data structure
             centroids_pre = deepcopy(centroids)
+            # calculate the k-mean(mean centroid) and update the centroids
             for k in range(self.k):
                 temp = X[clusters == k]
                 if len(temp) != 0:
                     centroids[k] = np.mean(temp, axis=0)                
             iter_Num += 1
+            # check if converge
             if np.linalg.norm(centroids - centroids_pre) == 0:
                 break
         return clusters, centroids
