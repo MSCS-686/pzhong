9a05faeae85386467282344e9498b95ff1098348
PeilinZhong <31450172+PeilinzhongZ@users.noreply.github.com>
2019-04-15 18:10:08 -0700

    update 3

--- Lab03/perceptron.py
+++ Lab03/perceptron.py
@@ -15,16 +15,16 @@ class perceptron(classifier):
                 hyp = self.activation(s)
                 self.weights[1:] += self.learning_rate * (y - hyp) * x
                 self.weights[0] += self.learning_rate * (y - hyp)
-
-    # def predict(self, X):
-    #     hyps = []
-    #     for x in X:
-    #         s = np.dot(x, self.weights[1:]) + self.weights[0]
-    #         hyps.append(self.activation(s))
-    #     return hyps
+        return self.weights
 
     def predict(self, X):
-        s = np.dot(X, self.weights[1:]) + self.weights[0]
+        hyps = []
+        for x in X:
+            hyps.append(self.predict_one(x))
+        return hyps
+
+    def predict_one(self, x):
+        s = np.dot(x, self.weights[1:]) + self.weights[0]
         return self.activation(s)
         
     def activation(self, x):
